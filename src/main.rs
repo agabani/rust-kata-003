4fce6d4391262fb922cfc3647a80b7150970f532
agabani <agabani@users.noreply.github.com>
2021-02-05 19:12:53 +0000

    refactor: startup

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,7 @@
-use rust_kata_003::configuration::Configuration;
 use rust_kata_003::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let configuration = Configuration::load().expect("Failed to read configuration.");
-
-    let postgres_pool = configuration.postgres.server_pool();
-    let redis_pool = configuration.redis.connection_manager().await;
-    let listener = configuration.http_server.tcp_listener();
-
-    let server = run(listener, postgres_pool, redis_pool).expect("Failed to bind address.");
+    let (server, _) = run().await;
     server.await
 }
